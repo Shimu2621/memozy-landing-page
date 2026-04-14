f86b2a828c7e12b01b31938a679c9d1e52621211
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-04-13 22:47:01 -0400

    update file

--- src/components/pages/HowToUse/StayOnSchedule.tsx
+++ src/components/pages/HowToUse/StayOnSchedule.tsx
@@ -83,7 +83,7 @@ export default function StayOnSchedule() {
               <p className="text-lg md:text-xl">
                 Stay on top of your busy schedule with Memozy. Views and manages
                 your appointments, meetings, and events in one place. Never miss
-                an important deadline or event again.
+                an important deadline or events again.
               </p>
             </motion.div>
 
