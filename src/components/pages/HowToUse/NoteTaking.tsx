6e9044f90cba404ae5b650635f2671bd615f4b91
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-17 00:00:27 -0400

    update file

--- src/components/pages/HowToUse/NoteTaking.tsx
+++ src/components/pages/HowToUse/NoteTaking.tsx
@@ -77,7 +77,7 @@ export function NoteTaking() {
             >
               <p className=" text-base md:text-lg leading-relaxed">
                 <span className=" font-semibold">Example:</span> 🗣️
-                &quot;Memozy, save a note: Brainstorm ideas for my next
+                &quot;Memozy, save a note: Brainstorm idea for my next
                 projects.&quot;
               </p>
             </motion.div>
@@ -95,7 +95,7 @@ export function NoteTaking() {
                     icon: Mic,
                     title: "Voice Notes",
                     description:
-                      "Quickly capture your thoughts with voice-to-text transcriptions.",
+                      "Quickly capture your thoughts with voice-to-text transcription.",
                   },
                   {
                     icon: Search,
