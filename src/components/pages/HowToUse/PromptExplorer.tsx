45cf9ba8d5068890f08ad6e4c668ebb81f1a2321
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-17 23:39:39 -0400

    update file

--- src/components/pages/HowToUse/PromptExplorer.tsx
+++ src/components/pages/HowToUse/PromptExplorer.tsx
@@ -139,7 +139,7 @@ export default function PromptExplorer() {
               variants={isMobile ? {} : itemVariants}
               className="space-y-4"
             >
-              <h3 className="text-xl md:text-2xl font-bold text-slate-800 dark:text-white">
+              <h3 className="text-xl md:text-2xl font-bold text-slate-800 dark:text-white font-outfit">
                 Explore Prompts For:
               </h3>
 
