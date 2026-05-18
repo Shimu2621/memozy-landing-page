c20030534ef844fad17730b81c0ec58bb4a7addc
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-17 23:41:43 -0400

    update file

--- src/components/pages/HowToUse/SmartReminders.tsx
+++ src/components/pages/HowToUse/SmartReminders.tsx
@@ -111,7 +111,7 @@ export default function SmartReminders() {
               </h2>
               <p className="text-lg md:text-xl text-slate-800 dark:text-blue-100 font-outfit">
                 Never forget important tasks or events again. Memozy&apos;s
-                smart reminders help you stay organized and on track. Set
+                smart reminder help you stay organized and on track. Set
                 reminders for appointments, deadlines, and other important
                 moment with ease.
               </p>
