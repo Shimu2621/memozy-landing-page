8d0bd40c6d60fd3de1e7c1c9e81cfa47d75d30a7
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-05-22 23:49:48 -0400

    update file

--- src/components/pages/HowToUse/HowToUsePage.tsx
+++ src/components/pages/HowToUse/HowToUsePage.tsx
@@ -102,7 +102,7 @@ export const HowToUsePage = () => {
       {
         "@type": "HowToStep",
         name: "Prompt Explorer",
-        text: "Unlock your creativity with a universe of prompt. For example, say 'Give me a tarot reading for my current situations.'",
+        text: "Unlock your creativity with a universe of prompt. For example, say 'Give me a tarot reading for my current situation.'",
         url: "https://www.memozy.ai/how-to-use-memozy.html#prompt-explorer",
       },
     ],
